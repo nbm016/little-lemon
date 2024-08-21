1c2811d74f373335a898bc103b8141a36ccdee58
unknown <nivibmunjal@gmail.com>
2024-08-21 19:15:32 -0400

    Comments and CSS

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+//import BookingForm from './components/BookingForm'
+//import Header from './components/Header'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the header', () => {
+  render(<BrowserRouter><App/></BrowserRouter>);
+  const headerElement = screen.getByText("This week's specials!");
+  expect(headerElement).toBeInTheDocument();
+
+  const bookingButton = screen.getByRole("button");
+  fireEvent.click(bookingButton);
+
+  const bookingPage = screen.getByText("Choose Date");
+  expect(bookingPage).toBeInTheDocument();
 });
